23b67b9da7e3e64552b09c003cb0b78add47834e
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2024-04-04 19:27:59 -0400

    lego is not a good product because they don't have good 3d assets

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import * as Comps from "@/components/ui";
 import TwoByFourBrick from "@/components/models/twoByFourBrick";
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 export default function App() {
   return (
     <>
       <Comps.Button>Kevin</Comps.Button>
-      <Suspense fallback={<>Its Loading. Be patient Kevin!</>}>
-        <Canvas>
+      <Canvas>
+        <Suspense fallback={<>Its Loading. Be patient Kevin!</>}>
+          <OrbitControls />
           <TwoByFourBrick />
-        </Canvas>
-      </Suspense>
+        </Suspense>
+      </Canvas>
     </>
   );
 }
--- src/components/models/twoByFourBrick.tsx
+++ src/components/models/twoByFourBrick.tsx
@@ -2,7 +2,7 @@ import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/Addons.js";
 
 export default function Model() {
-  const result = useLoader(GLTFLoader, "./scene.gltf");
+  const result = useLoader(GLTFLoader, "./new.gltf");
 
   return <primitive object={result.scene} />;
 }
